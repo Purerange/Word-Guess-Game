0fc004a45356abed6615b6a87ca4341c3e1a3fef
Stephen Marsh <38229184+Purerange@users.noreply.github.com>
2018-09-08 19:21:22 -0700

    created basic wireframe and array of possible words

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+
+var possibleWords = [
+    "THE DOCTOR",
+    "THE MASTER",
+    "DALEK",
+    "TARDIS",
+    "CYBERMAN",
+    "SONTARAN",
+    "GALLIFREY",
+    "ZYGON",
+    "COMPANION",
+    "TORCHWOOD",
+    "SONIC SCREWDRIVER",
+    "EXTERMINATE",
+    "ROSE TYLER",
+    "BAD WOLF",
+    "TIME LORD",
+    "BOW TIE",
+    "RIVER SONG",
+    "FANTASTIC",
+    "ALLONS Y",
+    "GERONIMO",
+    "TIME VORTEX",
+    "JACK HARKNESS",
+    "DONNA NOBLE",
+    "MARTHA JONES",
+    "AMY POND",
+    "RORY WILLIAMS",
+    "CLARA OSWALD",
+    "NARDOLE",
+    "BILL POTTS",
+    "THE SILENCE",
+    "SLITHEEN",
+    "WEEPING ANGEL"
+]
+
+$(document).ready(function() {
+
+})
