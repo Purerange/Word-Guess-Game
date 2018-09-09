6c7e50904c8aa2e41cb5f1705241145b8ee78854
Stephen Marsh <38229184+Purerange@users.noreply.github.com>
2018-09-08 20:13:48 -0700

    added base refresh functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -34,6 +34,70 @@ var possibleWords = [
     "WEEPING ANGEL"
 ]
 
+var wordArray = [];
+var displayWord = "";
+var remainingGuesses = 15;
+var lettersGuessed = "";
+var wins = 0;
+
+function newWord(){
+    return possibleWords[Math.floor(Math.random() * possibleWords.length)];
+}
+
+function insertWord(word) { 
+
+    for (let i = 0; i < word.length; i++) {
+        wordArray.push(word[i]);
+    }
+    console.log(wordArray);
+
+    for (let j = 0; j < wordArray.length; j++) {
+        if (wordArray[j] === " ") {
+            displayWord = displayWord + "  ";
+        } else {
+            displayWord = displayWord + " _";
+        }
+    }
+    console.log(displayWord);
+
+    updateDisplay(displayWord);
+}
+
+function updateWins(wins) {
+    $("#wins").text(wins);
+}
+
+function updateDisplay(displayWord) {
+    $("#displayWord").html("<pre>" + displayWord + "</pre>");
+}
+
+function updateGuesses(remainingGuesses) {
+    $("#remainingGuesses").text(remainingGuesses);
+}
+
+function updateLettersGuessed(lettersGuessed) {
+    $("#lettersGuessed").html("<pre>" + lettersGuessed + "</pre>");
+}
+
 $(document).ready(function() {
 
+    var word = newWord();
+
+    console.log("Your word is: " + word);
+
+    updateWins(wins);
+
+    insertWord(word);
+
+    updateGuesses(remainingGuesses);
+
+    updateLettersGuessed(lettersGuessed);
+    
+})
+
+$(document).keyup(function(e) {
+
+    // Need to figure out why this equals "t" when F5 is released.
+    var keyPressed = String.fromCharCode(e.keyCode);
+    console.log(keyPressed);
 })
